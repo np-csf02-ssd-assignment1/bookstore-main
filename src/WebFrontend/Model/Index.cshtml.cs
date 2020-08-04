0bc8a7343d5147f5e09c46ad8b8cdcfba8581335
dodieboy <dodieboy@users.noreply.github.com>
2020-08-04 15:00:57 +0800

    Add product model

---
+++ src/WebFrontend/Model/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebFrontend.Data;
+
+namespace WebFrontend.Model
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebFrontend.Data.WebFrontendContext _context;
+
+        public IndexModel(WebFrontend.Data.WebFrontendContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Product.ToListAsync();
+        }
+    }
+}
