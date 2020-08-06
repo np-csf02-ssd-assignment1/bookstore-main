f406542124a42bb40c95c9cc9c5f517e06a16390
dodieboy <dodieboy@users.noreply.github.com>
2020-08-06 19:02:09 +0800

    Add input validation

--- src/WebFrontend/Model/Product.cs
+++ src/WebFrontend/Model/Product.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace WebFrontend.Model
     public class Product
     {
         public int ID { get; set; }
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter a valid name")]
         public string Name { get; set; }
+        [RegularExpression("^[a-zA-Z., ]*$", ErrorMessage = "Please enter a valid description")]
         public string Description { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
