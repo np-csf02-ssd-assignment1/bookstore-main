a9da174385d35390aff56fdcbacb1d05d6c7a9b7
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2020-08-08 16:19:44 +0800

    feat: adds strict emailsender enable
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- src/WebFrontend/Services/EmailSender/SendGrid/ServicesConfiguration.cs
+++ src/WebFrontend/Services/EmailSender/SendGrid/ServicesConfiguration.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
@@ -5,10 +6,14 @@ namespace WebFrontend.Services.EmailSender.SendGrid
 {
     public static class ServicesConfiguration
     {
-        public static void AddSendGridEmailSender(this IServiceCollection services, AuthMessageSenderOptions options)
+        public static void AddSendGridEmailSender(this IServiceCollection services, IConfiguration configure)
         {
+            services
+                .AddOptions<AuthMessageSenderOptions>()
+                .Bind(configure)
+                .ValidateDataAnnotations();
+
             services.AddTransient<IEmailSender, EmailSender>();
-            services.AddOptions<AuthMessageSenderOptions>();
         }
     }
 }
