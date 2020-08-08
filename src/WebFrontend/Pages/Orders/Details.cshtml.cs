9834b6bf0d22487256cebd0b8f90a2a264970b4c
dodieboy <dodieboy@users.noreply.github.com>
2020-08-08 10:44:20 +0800

    Order index will only show user order

--- src/WebFrontend/Pages/Orders/Details.cshtml.cs
+++ src/WebFrontend/Pages/Orders/Details.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using WebFrontend.Model;
 
 namespace WebFrontend.Pages.Orders
 {
+    [Authorize(Roles = "Admin, User")]
     public class DetailsModel : PageModel
     {
         private readonly WebFrontend.Data.WebFrontendContext _context;
@@ -28,7 +30,7 @@ namespace WebFrontend.Pages.Orders
                 return NotFound();
             }
 
-            Order = await _context.Order.FirstOrDefaultAsync(m => m.OrderID == id);
+            Order = await _context.OrderList.FirstOrDefaultAsync(m => m.OrderID == id);
 
             if (Order == null)
             {
--- src/WebFrontend/Pages/Orders/Index.cshtml.cs
+++ src/WebFrontend/Pages/Orders/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +13,7 @@ using WebFrontend.Model;
 
 namespace WebFrontend.Pages.Orders
 {
+    [Authorize(Roles = "Admin, User")]
     public class IndexModel : PageModel
     {
         private readonly WebFrontend.Data.WebFrontendContext _context;
@@ -21,13 +23,15 @@ namespace WebFrontend.Pages.Orders
             _context = context;
         }
 
+        public string SQLmessage { get; private set; }
         public IList<Order> Order { get; set; }
         public string UserID { get; set; }
 
         public async Task OnGetAsync()
         {
-            Order = await _context.Order.ToListAsync();
             UserID = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            SQLmessage = "Select * From OrderList where UserId ='" + UserID + "'";
+            Order = await _context.OrderList.FromSqlRaw(SQLmessage).ToListAsync();
         }
     }
 }
